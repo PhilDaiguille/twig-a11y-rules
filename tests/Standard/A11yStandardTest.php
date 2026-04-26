352dd07d227213e4f4498d7fd841e614960b1743
PhilDaiguille <philippe.delente@gmail.com>
2026-04-26 14:43:55 +0200

    fix phpstan

--- tests/Standard/A11yStandardTest.php
+++ tests/Standard/A11yStandardTest.php
@@ -5,12 +5,31 @@ declare(strict_types=1);
 namespace TwigA11y\Tests\Standard;
 
 use PHPUnit\Framework\TestCase;
+use TwigA11y\Rules\Aria\AriaHiddenFocusRule;
+use TwigA11y\Rules\Aria\AriaLabelRule;
+use TwigA11y\Rules\Aria\AriaRequiredAttrRule;
+use TwigA11y\Rules\Aria\AriaRoleRule;
+use TwigA11y\Rules\Aria\TabIndexRule;
+use TwigA11y\Rules\Forms\FormLabelRule;
+use TwigA11y\Rules\Forms\InputLabelRule;
+use TwigA11y\Rules\Forms\SelectLabelRule;
+use TwigA11y\Rules\Forms\TextareaLabelRule;
+use TwigA11y\Rules\Media\AutoplayRule;
 use TwigA11y\Rules\Media\ImgAltRule;
+use TwigA11y\Rules\Media\ObjectAltRule;
+use TwigA11y\Rules\Structure\AnchorContentRule;
 use TwigA11y\Rules\Structure\BannedTagsRule;
+use TwigA11y\Rules\Structure\ButtonContentRule;
+use TwigA11y\Rules\Structure\HeadingEmptyRule;
 use TwigA11y\Rules\Structure\HeadingOrderRule;
+use TwigA11y\Rules\Structure\IframeTitleRule;
+use TwigA11y\Rules\Structure\LangAttributeRule;
+use TwigA11y\Rules\Structure\MetaViewportRule;
 use TwigA11y\Standard\A11yBasicStandard;
 use TwigA11y\Standard\A11yRecommendedStandard;
 use TwigA11y\Standard\A11yStandard;
+use TwigA11y\Standard\A11yStrict;
+use TwigCsFixer\Rules\RuleInterface;
 
 /**
  * @internal
@@ -24,6 +43,9 @@ final class A11yStandardTest extends TestCase
         $standard = new A11yBasicStandard();
         $rules = $standard->getRules();
         $this->assertNotEmpty($rules);
+        foreach ($rules as $rule) {
+            $this->assertInstanceOf(RuleInterface::class, $rule);
+        }
         $classes = array_map(fn ($r): string => $r::class, $rules);
         $this->assertContains(ImgAltRule::class, $classes);
         $this->assertContains(BannedTagsRule::class, $classes);
@@ -49,5 +71,36 @@ final class A11yStandardTest extends TestCase
         $this->assertContains(ImgAltRule::class, $classes);
         $this->assertContains(BannedTagsRule::class, $classes);
         $this->assertContains(HeadingOrderRule::class, $classes);
+    }
+
+    public function testA11yStrictProvidesRules(): void
+    {
+        $standard = new A11yStrict();
+        $rules = $standard->getRules();
+
+        $this->assertNotEmpty($rules);
+
+        $classes = array_map(static fn (RuleInterface $rule): string => $rule::class, $rules);
+
+        $this->assertContains(ImgAltRule::class, $classes);
+        $this->assertContains(AutoplayRule::class, $classes);
+        $this->assertContains(ObjectAltRule::class, $classes);
+        $this->assertContains(BannedTagsRule::class, $classes);
+        $this->assertContains(ButtonContentRule::class, $classes);
+        $this->assertContains(AnchorContentRule::class, $classes);
+        $this->assertContains(HeadingOrderRule::class, $classes);
+        $this->assertContains(HeadingEmptyRule::class, $classes);
+        $this->assertContains(LangAttributeRule::class, $classes);
+        $this->assertContains(IframeTitleRule::class, $classes);
+        $this->assertContains(MetaViewportRule::class, $classes);
+        $this->assertContains(TabIndexRule::class, $classes);
+        $this->assertContains(AriaRoleRule::class, $classes);
+        $this->assertContains(AriaLabelRule::class, $classes);
+        $this->assertContains(AriaHiddenFocusRule::class, $classes);
+        $this->assertContains(AriaRequiredAttrRule::class, $classes);
+        $this->assertContains(FormLabelRule::class, $classes);
+        $this->assertContains(InputLabelRule::class, $classes);
+        $this->assertContains(SelectLabelRule::class, $classes);
+        $this->assertContains(TextareaLabelRule::class, $classes);
     }
 }
